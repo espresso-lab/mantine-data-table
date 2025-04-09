6106a7eed3fc407cbc0c1265123a36711d8e469d
Jannick <jannick@viets.email>
2025-04-09 21:08:44 +0200

    fix: Mantine Auth

--- src/Context/DataTableContext.tsx
+++ src/Context/DataTableContext.tsx
@@ -1,4 +1,4 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createContext, ReactNode } from "react";
 
 interface State {
@@ -21,7 +21,7 @@ export function DataTableProvider({
 }: DataTableContextProps) {
   return (
     <DataTableContext.Provider value={{ baseUrl, queryClient }}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </DataTableContext.Provider>
   );
 }
