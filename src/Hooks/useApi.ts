73cc4a1d73cfcbb6c3c1bd12bafc5b993187bb98
Jannick <jannick@viets.email>
2025-04-09 19:54:00 +0200

    fix: Mantine Auth

--- src/Hooks/useApi.ts
+++ src/Hooks/useApi.ts
@@ -1,12 +1,8 @@
-import {
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-import {getIdToken} from "@espresso-lab/mantine-cognito";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getIdToken } from "@espresso-lab/mantine-cognito";
 
 export interface BaseEntity {
-    id: string | number;
+  id: string | number;
 }
 
 type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>;
@@ -29,35 +25,38 @@ export async function getAll<T extends BaseEntity>(path: string): Promise<T[]> {
     method: "GET",
     headers: await getApiHeaders(),
   })
-      .then(async (resp) => {
-        if (resp.status >= 400) {
-          throw await resp.text();
-        }
-        return resp;
-      })
-      .then((resp) => resp.json())
-      .then((data) => data as T[]);
+    .then(async (resp) => {
+      if (resp.status >= 400) {
+        throw await resp.text();
+      }
+      return resp;
+    })
+    .then((resp) => resp.json())
+    .then((data) => data as T[]);
 }
 
 export async function getOne<T extends BaseEntity>(
-    path: string,
-    id: string | number,
+  path: string,
+  id: string | number,
 ): Promise<T> {
   return fetch(`${path}/${id}`, {
     method: "GET",
     headers: await getApiHeaders(),
   })
-      .then(async (resp) => {
-        if (resp.status >= 400) {
-          throw await resp.text();
-        }
-        return resp;
-      })
-      .then((resp) => resp.json())
-      .then((data) => data as T);
+    .then(async (resp) => {
+      if (resp.status >= 400) {
+        throw await resp.text();
+      }
+      return resp;
+    })
+    .then((resp) => resp.json())
+    .then((data) => data as T);
 }
 
-export async function deleteOne(path: string, id: string | number): Promise<void> {
+export async function deleteOne(
+  path: string,
+  id: string | number,
+): Promise<void> {
   await fetch(`${path}/${id}`, {
     method: "DELETE",
     headers: await getApiHeaders(),
@@ -70,147 +69,147 @@ export async function deleteOne(path: string, id: string | number): Promise<void
 }
 
 export async function createOne<C, T extends BaseEntity>(
-    path: string,
-    item: C,
+  path: string,
+  item: C,
 ): Promise<T> {
   return fetch(path, {
     method: "POST",
     headers: await getApiHeaders(),
     body: JSON.stringify(item),
   })
-      .then(async (resp) => {
-        if (resp.status >= 400) {
-          throw await resp.text();
-        }
-        return resp;
-      })
-      .then((resp) => {
-        if (resp.status == 204) {
-          return item;
-        } else {
-          return resp.json();
-        }
-      })
-      .then((data) => data as T);
+    .then(async (resp) => {
+      if (resp.status >= 400) {
+        throw await resp.text();
+      }
+      return resp;
+    })
+    .then((resp) => {
+      if (resp.status == 204) {
+        return item;
+      } else {
+        return resp.json();
+      }
+    })
+    .then((data) => data as T);
 }
 
 export async function api<R, U>(
-    method: "GET" | "POST" | "PUT" | "DELETE",
-    path: string,
-    payload?: U,
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  path: string,
+  payload?: U,
 ): Promise<R> {
   return fetch(path, {
     method,
     headers: await getApiHeaders(),
     body: payload ? JSON.stringify(payload) : undefined,
   })
-      .then(async (resp) => {
-        if (resp.status >= 400) {
-          throw await resp.text();
-        }
-        return resp;
-      })
-      .then((resp) => {
-        if (resp.status == 204) {
-          return;
-        } else {
-          return resp.json();
-        }
-      })
-      .then((data) => data as R);
+    .then(async (resp) => {
+      if (resp.status >= 400) {
+        throw await resp.text();
+      }
+      return resp;
+    })
+    .then((resp) => {
+      if (resp.status == 204) {
+        return;
+      } else {
+        return resp.json();
+      }
+    })
+    .then((data) => data as R);
 }
 
 export async function updateOne<T extends BaseEntity>(
-    path: string,
-    item: AtLeast<T, "id">,
+  path: string,
+  item: AtLeast<T, "id">,
 ): Promise<T> {
   return fetch(`${path}/${item.id}`, {
     method: "PUT",
     headers: await getApiHeaders(),
     body: JSON.stringify(item),
   })
-      .then(async (resp) => {
-        if (resp.status >= 400) {
-          throw await resp.text();
-        }
-        return resp;
-      })
-      .then((resp) => {
-        if (resp.status == 204) {
-          return item;
-        } else {
-          return resp.json();
-        }
-      })
-      .then((data) => data as T);
+    .then(async (resp) => {
+      if (resp.status >= 400) {
+        throw await resp.text();
+      }
+      return resp;
+    })
+    .then((resp) => {
+      if (resp.status == 204) {
+        return item;
+      } else {
+        return resp.json();
+      }
+    })
+    .then((data) => data as T);
 }
 
 export function useGetOne<T extends BaseEntity>(
-    apiPath: string,
-    queryKey: Array<string | number>,
-    id: string | number,
+  apiPath: string,
+  queryKey: Array<string | number>,
+  id?: string | number,
 ) {
-  return  useQuery<T>({
-    queryKey: [...queryKey.map((k) => k.toString()), String(id.toString())],
+  return useQuery<T>({
+    queryKey: [...queryKey.map((k) => k.toString()), String(id?.toString())],
     queryFn: () => getOne<T>(apiPath, id!),
-  });}
+    enabled: !!id,
+  });
+}
 
 export function useGetAll<T extends BaseEntity>(
-    apiPath: string,
-    queryKey: Array<string | number>,
-    enabled = true,
+  apiPath: string,
+  queryKey: Array<string | number>,
 ) {
   return useQuery<T[]>({
     queryKey: [...queryKey.map((k) => k.toString())],
     queryFn: () => getAll<T>(apiPath),
-    enabled,
   });
 }
 
 export function useAddOne<T extends BaseEntity>(
-    apiPath: string,
-    queryKey: Array<string | number>,
+  apiPath: string,
+  queryKey: Array<string | number>,
 ) {
   const queryClient = useQueryClient();
   return useMutation<T, Error, Omit<T, "id">>({
     mutationKey: [...queryKey.map((k) => k.toString())],
     mutationFn: (item) => createOne<Omit<T, "id">, T>(apiPath, item),
-      onSettled() {
-          queryClient.invalidateQueries({
-              queryKey: [...queryKey.map((k) => k.toString())],
-          });
+    onSettled() {
+      queryClient.invalidateQueries({
+        queryKey: [...queryKey.map((k) => k.toString())],
+      });
     },
   });
 }
 
 export function useUpdateOne<T extends BaseEntity>(
-    apiPath: string,
-    queryKey: Array<string | number>,
+  apiPath: string,
+  queryKey: Array<string | number>,
 ) {
   const queryClient = useQueryClient();
   return useMutation<T, Error, AtLeast<T, "id">>({
     mutationKey: [...queryKey.map((k) => k.toString())],
     mutationFn: (item: AtLeast<T, "id">) => updateOne<T>(apiPath, item),
-      onSettled() {
-        queryClient.invalidateQueries({
-            queryKey: [...queryKey.map((k) => k.toString())],
-        });
+    onSettled() {
+      queryClient.invalidateQueries({
+        queryKey: [...queryKey.map((k) => k.toString())],
+      });
     },
   });
 }
 
 export function useDeleteOne(
-    apiPath: string,
-    queryKey: Array<string | number>,
+  apiPath: string,
+  queryKey: Array<string | number>,
 ) {
   const queryClient = useQueryClient();
   return useMutation<void, Error, string | number>({
     mutationKey: [...queryKey.map((k) => k.toString())],
     mutationFn: (id) => deleteOne(apiPath, id),
     onSettled() {
-        queryClient.invalidateQueries({
-            queryKey: [...queryKey.map((k) => k.toString())],
-        });
+      queryClient.invalidateQueries({
+        queryKey: [...queryKey.map((k) => k.toString())],
+      });
     },
   });
 }
