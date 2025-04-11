b4f02547c9dce7109a43c0dcb3125b2278e5d9b7
Jannick <jannick@viets.email>
2025-04-11 12:57:44 +0200

    fix: Skeleton

--- src/Hooks/useDataTable.ts
+++ src/Hooks/useDataTable.ts
@@ -4,7 +4,7 @@ import { DataTableContext, State } from "../Context/DataTableContext.tsx";
 export function useDataTable(): State {
   const context = useContext(DataTableContext);
 
-  if (context === undefined) {
+  if (!context) {
     throw new Error("useDataTable must be used within a DataTableProvider");
   }
 
